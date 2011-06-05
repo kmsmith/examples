134bd3f18fb7b753a255d4d360e741a11d4fdf1a
Kevin Smith <kevnsmth@gmail.com>
2011-06-05 02:41:02 -0400

    Added comments to atoi.c

--- c/atoi.c
+++ c/atoi.c
@@ -1,12 +1,21 @@
+/* Given two numbers on command line, output average */
+
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+
+/*
+ * Given a char array of digits, convert to an integer.
+ * Assumes given string is just digits.
+ */
 
 int a2i(char* num_str) {
     int len = strlen(num_str);
     int sum = 0;
     int i = 0;
     int mult = 0;
+
+    /* For 3 digit number, start multiplier at 100 and divide by 10 until 1 */
     for (mult = pow(10,len-1); mult >= 1; mult /= 10, i++) {
         sum += (num_str[i]-'0') * mult;
     }
